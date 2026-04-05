fbfd9e7e56e634bc3564221a7c9aa9818d672a99
Sabbir Ahmmed <srgamingzone71@gmail.com>
2026-04-06 02:15:31 +0600

    multer update

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -3,6 +3,6 @@ import multer from 'multer';
 export const multerUpload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 200 * 1024 * 1024, 
+    fileSize: 200 * 1024 * 1024
   },
 });
